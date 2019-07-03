7b4bbb726b374514128c2eb454ad603f7811b9cf
ryuki96 <51799058+ryuki96@users.noreply.github.com>
2019-07-03 10:37:50 +0900

    crate group commit
    グループ作成機能

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+
+  def edit
+  end
+
+  def update
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  def create
+    if user.save?
+      bypass_sign_in(user)
+      redirect_to root_path
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  root to: 'messages#index'
+  devise_for :users
+  root 'groups#index'
+  resources :users, only: [:edit, :update　,:create]
+  resources :groups, only: [:new, :create, :edit, :update] do
+    resources :messages, only: [:index, :create]
 end
